84bbc7a5faa447ce9d47b8e2f598db3c77dea12d
Donatien D <ddlu.web@icloud.com>
2022-08-23 11:38:20 +0200

    Speed in mph exercise

---
+++ 01-Speed-in-mph/lib/speed_in_mph.rb
@@ -0,0 +1,2 @@
+# TO DO: write a method that takes a float as an argument and
+# returns it converted from kph (km/h) to mph (mi/h)
---
+++ 01-Speed-in-mph/spec/speed_in_mph_spec.rb
@@ -0,0 +1,2 @@
+# TO DO: Write the specs that ensures our speed_in_mph method
+# successfully converts speed from km/h to mi/h.
